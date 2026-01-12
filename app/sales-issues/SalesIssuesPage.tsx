b97f41d4980879a9c6ee38b2063e8150524c4d8e
dyenta <mochammad.dyenta@ui.ac.id>
2026-01-12 14:14:48 +0700

    updated 12-01-26

--- app/sales-issues/SalesIssuesPage.tsx
+++ app/sales-issues/SalesIssuesPage.tsx
@@ -384,9 +384,9 @@ export default function SalesIssuesPage() {
                         className={`w-full px-4 py-2.5 bg-slate-50 dark:bg-slate-800 border border-slate-200 dark:border-slate-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all text-sm ${formData.issue_type === '' ? 'text-slate-400' : 'dark:text-white'}`}
                     >
                         <option value="" disabled>Pilih Issue Type</option>
-                        <option value="PO belum tersuplai">PO belum tersuplai</option>
+                        <option value="PO Belum Tersuplai">PO Belum Tersuplai</option>
                         <option value="Pengajuan Warranty">Pengajuan Warranty</option>
-                        <option value="Others">Others</option>
+                        <option value="OTHERS">OTHERS</option>
                     </select>
                 </div>
             </div>
